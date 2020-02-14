1e8f34003f7d727d95384d1e78fc0dbaa03fe2bf
Admin <Admin@DESKTOP-5QDD06O>
2020-02-14 21:01:38 +0700

    layout va model admin

---
+++ Flower_Project/Areas/Admin/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Flower_Project.Areas.Admin.Models
+{
+    public class Category
+    {
+        [Key]
+        public string CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public CategoryStatus Status { get; set; }
+
+        public enum CategoryStatus
+        {
+            Active = 1,
+            Deactivate = 0,
+            Deleted = -1
+        }
+    }
+}
